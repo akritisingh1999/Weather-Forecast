66e04c02dd89083b3ab19dfa0cfdd413d73be50b
akritisingh1999 <44137532+akritisingh1999@users.noreply.github.com>
2018-10-27 19:35:14 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,10 +1,2 @@
 var error = document.getElementById("wrong");
 var output = document.getElementById("output");
-if(error.textContent==="")
-{
-	error.style.padding="0";
-}
-if(output.textContent==="")
-{
-	output.style.padding="0";
-}
